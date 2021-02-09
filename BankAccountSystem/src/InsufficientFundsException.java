3e5a98540bacaab56c40fc0fab795956242d5c85
Saif Mahmud <60409889+saif81bd@users.noreply.github.com>
2021-02-09 10:45:17 -0600

    First commit

---
+++ BankAccountSystem/src/InsufficientFundsException.java
@@ -0,0 +1,14 @@
+
+/*Assignment of COMP 1020 
+ * Saif Mahmud
+ * ID: 7808507
+ */
+public class InsufficientFundsException extends Exception {
+	
+	public InsufficientFundsException() {
+		
+	}
+	public String toString() {
+		return ("Insufficient funds for the withdrawal");
+	}
+}
